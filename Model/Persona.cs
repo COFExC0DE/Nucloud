64a11d450d5cdaa3583e5bf8a4c1c37b091208b7
COFExC0DE <rocksjos1993@hotmail.com>
2020-09-30 15:07:00 -0700

    Update Persona.cs

--- Model/Persona.cs
+++ Model/Persona.cs
@@ -9,6 +9,6 @@ namespace Sala.Model {
         public int ID { get; set; }
         public string Nombre { get; set; }
         public int Telefono { get; set; }
-        public string email { get; set; }
+        public string Email { get; set; }
     }
 }
