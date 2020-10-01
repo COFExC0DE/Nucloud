8577a127ba0eadae437d6e1ffde6c63197b929bd
COFExC0DE <rocksjos1993@hotmail.com>
2020-09-30 19:47:38 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Resources;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Sala.Controller;
@@ -11,14 +13,26 @@ namespace Sala {
     class Program {
 
         static void Main(string[] args) {
+
+
             bool repetir = true;
             List<Persona> customr = new List<Persona>(1000);
             List<Servicio> service = new List<Servicio>(1000);
+
+            GestionarCliente g_Cliente = new GestionarCliente();// esto es de prueba
+            bool f = false;// esto es de prueba
+            bool t = true;// esto es de prueba
+            Cliente add_Cliente = new Cliente()// esto es de prueba
+            {
+                Moroso = t,// esto es de prueba
+                Activo = f// esto es de prueba
+            };// esto es de prueba
+
             do
             {
 
                 Console.WriteLine("\n 1) Crear servicio para la sala");
-                Console.WriteLine("2) Ingresar un nuevo miembro de la sala");
+                Console.WriteLine("2) Ingresar un nuevo cliente de la sala");
                 Console.WriteLine("3) Ver los servicios de la sala");
                 Console.WriteLine("4) Ver miembros de la sala");
 
@@ -39,14 +53,23 @@ namespace Sala {
                 }
                 if (valor_Tomado == 2)
                 {
+                    int I = Int32.Parse(Console.ReadLine());
+                    string N = Console.ReadLine();
+                    int T = Int32.Parse(Console.ReadLine());
+                    string E = Console.ReadLine();
+
                     Console.WriteLine("\n Ingresar ID, Nombre, Telefono y Email (En ese orden)");
                     Persona add_Persona = new Persona()
                     {   
-                        ID = Int32.Parse(Console.ReadLine()),
-                        Nombre = Console.ReadLine(),
-                        Telefono = Int32.Parse(Console.ReadLine()),
-                        Email = Console.ReadLine()
+                        ID = I,
+                        Nombre = N,
+                        Telefono = T,
+                        Email = E
                     };
+                    add_Cliente.ID = I;// esto es de prueba
+                    g_Cliente.addClientes(add_Cliente);// esto es de prueba
+
+
                     customr.Add(add_Persona);
                     Console.WriteLine("\n Se guardo el nuevo Cliente");
                 }
