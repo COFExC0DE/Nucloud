b4ed6de4ce03e9c33e74c4cb925ca5da52b4a77e
COFExC0DE <rocksjos1993@hotmail.com>
2020-10-02 17:15:35 -0700

    Add files via upload

---
+++ Controller/EncargadoServicio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sala.Model;
+
+namespace Sala.Controller {
+    class EncargadoServicio {
+        public List<Servicio> Servicios { get; set; }
+
+        public EncargadoServicio() {
+            Servicios = new List<Servicio>();
+        }
+
+        public void agregarServicio(int cod, string desc) {
+            Servicios.Add(new Servicio() {
+                Codigo = cod,
+                Descripcion = desc
+            });
+        }
+
+        public Servicio obtenerServicio(int cod) {
+            return Servicios.Find(x => x.Codigo == cod);
+        }
+    }
+}
