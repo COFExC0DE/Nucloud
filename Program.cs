5d849624dfb15c7a0a829611e89fc26f056066ea
Andrés <nosoysaenz@gmail.com>
2020-11-07 22:37:33 -0600

    tenemos conexion

--- Program.cs
+++ Program.cs
@@ -6,11 +6,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NuCloudWeb.Controllers;
 
 namespace NuCloudWeb {
     public class Program {
         public static void Main(string[] args) {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();     
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
