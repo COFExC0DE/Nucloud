3305691f7deb1ef776f60fcf9edde3222e03cdc3
Andrés <nosoysaenz@gmail.com>
2020-11-07 20:31:38 -0600

    conexion

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -10,7 +11,13 @@ using Microsoft.Extensions.Logging;
 namespace NuCloud {
     public class Program {
         public static void Main(string[] args) {
+            DB.Instance.SetDriver("neo4j://localhost:7687", "neo4j", "1234");
             CreateHostBuilder(args).Build().Run();
+        }
+
+        public static async Task DoShit() {
+            List<string> t = await DB.Instance.GetAsync();
+            t.ForEach(x => Console.WriteLine(x));
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
