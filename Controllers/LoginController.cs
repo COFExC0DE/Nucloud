24f0e297b0cd905ad619b621b2b23a2711ab7240
Gerarld <gerarld73@gmail.com>
2021-01-16 01:43:40 -0600

    Una maquina de matar de login
    Se agregan lineas de maquinas de matar para logins, ignorar como cerrar sesion :o

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using NuCloudWeb.Models;
+
+namespace NuCloudWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        //View to login member
+        public IActionResult LoginMember()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //cuek
+        public async Task<IActionResult> LoginMember(Member member)
+        {
+            long num = DB.Instance.CantMemberForCedAndPass(member.Ced, member.Password).Result;
+            if (num == 1)
+            {
+                var miembro = DB.Instance.GetMember(member.Ced);
+                var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, member.Ced.ToString()));
+                identity.AddClaim(new Claim(ClaimTypes.Name, miembro.Result.Name));
+                var principal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal,
+                    new AuthenticationProperties { ExpiresUtc = DateTime.Now.AddDays(1), IsPersistent = true });
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Caca fallo");
+            }
+            return View();
+        }
+    }
+}
--- Controllers/MemberController.cs
+++ Controllers/MemberController.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Neo4j.Driver;
 using NuCloudWeb.Models;
 
 namespace NuCloudWeb.Controllers {
+
+    [Authorize]
+
     public class MemberController : Controller {
         public IActionResult Index() {
             return View();
@@ -47,27 +54,5 @@ namespace NuCloudWeb.Controllers {
             return View();
         }
 
-        [HttpGet]
-        //View to login member
-        public IActionResult LoginMember()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        //cuek
-        public IActionResult LoginMember(Member member)
-        {
-            long num = DB.Instance.CantMemberForCedAndPass(member.Ced, member.Password).Result;
-            if(num == 1)
-            {
-                System.Diagnostics.Debug.WriteLine("Caca");
-            }
-            else
-            {
-                System.Diagnostics.Debug.WriteLine("Caca fallo");
-            }
-            return View();
-        }
     }
 }
